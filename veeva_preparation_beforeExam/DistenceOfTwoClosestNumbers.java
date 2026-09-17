74ebe514578d279fffbc8f8c2ccf111a8de13606
bhargav <bhargavbala56@gmail.com>
2026-09-17 21:48:31 +0530

    .

--- veeva_preparation_beforeExam/DistenceOfTwoClosestNumbers.java
+++ veeva_preparation_beforeExam/DistenceOfTwoClosestNumbers.java
@@ -4,6 +4,10 @@ public class DistenceOfTwoClosestNumbers {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter n");
         int n=sc.nextInt();
+
+
+
+        
         System.out.println("Enter elments");
         int arr[]=new int[n];
         for(int i=0;i<n;i++)
