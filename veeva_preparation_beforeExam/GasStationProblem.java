7c9f479c5f212f3a3c9b6f36edfbff4c1ceab781
bhargav <bhargavbala56@gmail.com>
2026-09-19 21:26:16 +0530

    .

---
+++ veeva_preparation_beforeExam/GasStationProblem.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+       int start=0,curramount=0,totalgas=0,totalcost=0;
+
+       for(int i=0;i<gas.length;i++)
+       {
+        totalgas+=gas[i];
+        totalcost+=cost[i];
+        curramount += gas[i]-cost[i];
+        if(curramount<0)
+        {
+            start=i+1;
+            curramount=0;
+        }
+       }
+       if(totalcost>totalgas) return -1;
+       return start;
+
+        
+        
+    }
+}
